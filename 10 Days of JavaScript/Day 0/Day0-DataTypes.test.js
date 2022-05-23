2c67ff54663c005f99ff87295a4c7a87d2e13e98
Fremen1990 <fremen1990@gmail.com>
2022-05-23 02:11:24 +0200

    Day0-DataTypes challange.

---
+++ 10 Days of JavaScript/Day 0/Day0-DataTypes.test.js
@@ -0,0 +1,8 @@
+import {performOperation} from "./Day0-DataTypes";
+
+
+test("If DataTypes challenge passes basic test", ()=>{
+	performOperation(12, 4.32, "is the best place to learn and practice coding!")
+
+
+})
