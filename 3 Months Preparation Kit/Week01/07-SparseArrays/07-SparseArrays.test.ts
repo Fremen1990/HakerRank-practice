0484d51f7174edc6e3f8267685b83778883db50f
DevThomas <fremen1990@gmail.com>
2022-08-24 00:13:46 +0200

    3 Month Preparation KIT - 1 Week - 07-SparseArrays

---
+++ 3 Months Preparation Kit/Week01/07-SparseArrays/07-SparseArrays.test.ts
@@ -0,0 +1,3 @@
+describe("3 Months Preparation Kit - Week01 - 07-SparseArrays", ()=>{
+    it.todo("check if sparse array is properly solved")
+})
---
+++ 3 Months Preparation Kit/Week01/07-SparseArrays/07-SparseArrays.ts
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-sparse-arrays/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    // Write your code here
+    const memory = {}
+    for(const str of strings) memory[str] = (memory[str] || 0 ) +1;
+    return queries.map((query) => memory[query] || 0)
+}
